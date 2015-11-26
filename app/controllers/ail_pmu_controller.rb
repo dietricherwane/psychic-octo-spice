bcde56f094bda3893c1da7282cbe3a4b1135606b
Dietrich <dietricherwane@yahoo.fr>
2015-11-26 12:25:25 +0000

    changes

--- app/controllers/ail_pmu_controller.rb
+++ app/controllers/ail_pmu_controller.rb
@@ -268,7 +268,7 @@ class AilPmuController < ApplicationController
 
                 @ail_pmu = AilPmu.create(transaction_id: @transaction_id, message_id: @message_id, audit_number: @audit_id, date_time: @date_time, bet_code: @bet_code, bet_modifier: @bet_modifier, selector1: @selector1, selector2: @selector2, repeats: @repeats, normal_entries: @normal_entries, special_entries: @special_entries, ticket_number: ticket_number, ref_number: ref_number, bet_cost_amount: bet_cost_amount, bet_payout_amount: bet_payout_amount, paymoney_account_number: paymoney_account_number, gamer_id: gamer_id, user_id: user.id)
 
-                if debit_paymoney_account(paymoney_account_number, bet_cost_amount)
+                if true #debit_paymoney_account(paymoney_account_number, bet_cost_amount)
                   api_acknowledge_bet_old
                 end
 
