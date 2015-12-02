0320efa415f34dce45bb97ee7181dadcdaa637df
Dietrich <dietricherwane@yahoo.fr>
2015-12-02 09:28:39 +0000

    changes

--- app/controllers/ail_loto_controller.rb
+++ app/controllers/ail_loto_controller.rb
@@ -755,7 +755,7 @@ class AilLotoController < ApplicationController
       @error_code = '5000'
       @error_description = 'Invalid JSON data.'
     else
-      @bet = (AilLoto.where(audit_number: (notification_object["AuditId"].to_s rescue ""), ref_number: (notification_object["RefNumber"].to_s rescue ""), ticket_number: (notification_object["TicketNumber"].to_s rescue ""))
+      @bet = (AilLoto.where(audit_number: (notification_object["AuditId"].to_s rescue ""), ref_number: (notification_object["RefNumber"].to_s rescue ""), ticket_number: (notification_object["TicketNumber"].to_s rescue "")) rescue nil)
       if @bet.blank?
         @error_code = '4000'
         @error_description = 'The transaction could not be found'
