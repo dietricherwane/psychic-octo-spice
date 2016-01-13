be6faecf5daf50910e3a3aa0f23f8f186a6fb6b9
Dietrich Mensah <dietricherwane@yahoo.fr>
2016-01-13 13:09:40 +0100

    changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,29 +10,18 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.action_mailer.default_url_options = { host: '41.189.40.193:10000', from: 'Asministrateur - Parions Direct' }
-
-	ActionMailer::Base.smtp_settings = {
-  	:openssl_verify_mode => 'none',
-		:address => "smtp.gmail.com",
-		:enable_starttls_auto => true,
-		:port => 587,
-		:authentication => :plain,
-		:user_name => "mensah.dietrich@gmail.com",
-		:password => '20dianapopoulos12'
-	}
-=begin
-	config.action_mailer.default_url_options = { host: '41.189.40.193:10000', from: 'Asministrateur - Parions Direct' }
+config.action_mailer.default_url_options = { :host => '154.68.45.82:1180' }
   ActionMailer::Base.smtp_settings = {
-  	:openssl_verify_mode => 'none',
-		:address => "de2608.ispfr.net",
-		:enable_starttls_auto => true,
-		:port => 587,
-		:authentication => :plain,
-		:user_name => "serviceclients@de2608.ispfr.net",
-		:password => 'clients#2015'
-	}
-=end
+        :openssl_verify_mode => "none",
+                :address => "de2608.ispfr.net",
+                :enable_starttls_auto => true,
+                :port => 587,
+                :authentication => :plain,
+                :user_name => "serviceclients@de2608.ispfr.net",
+                :password => "clients#2015"
+        }
+
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
