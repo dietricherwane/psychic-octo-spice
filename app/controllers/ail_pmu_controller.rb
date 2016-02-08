6e59e2908424c06d5d85402f8deee75909fe974e
Dietrich <dietricherwane@yahoo.fr>
2016-02-08 13:40:22 +0000

    changes

--- app/controllers/ail_pmu_controller.rb
+++ app/controllers/ail_pmu_controller.rb
@@ -963,8 +963,8 @@ class AilPmuController < ApplicationController
   end
 
   def api_last_request_log
-    @previous_operation = AilPmuLog.find(AilLotoLog.last.id - 1)
-    @last_operation = AilPmuLog.last
+    @previous_operation = AilPmuLog.find(AilLotoLog.last.id - 1) rescue ""
+    @last_operation = AilPmuLog.last rescue ""
 
     render text: "---Operation: " + (@previous_operation.operation || "") + "\n\n---Transaction ID: " +  (@previous_operation.transaction_id || "") + "\n\n---Sent params: " +  (@previous_operation.sent_params || "") + "\n\n---Response: " +  (@previous_operation.response_body || "") + "\n\n\n\n---Operation: " + (@last_operation.operation || "") + "\n\n---Transaction ID: " + (@last_operation.transaction_id || "") + "\n\n---Sent params: " + (@last_operation.sent_params || "") + "\n\n---Response: " + (@last_operation.response_body || "")
   end
