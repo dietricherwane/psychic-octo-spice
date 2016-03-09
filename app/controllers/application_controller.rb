5621c987b2490e9a59e1ba5a4c26bccebf23b079
Dietrich <dietricherwane@yahoo.fr>
2016-03-09 23:14:06 +0000

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -421,4 +421,59 @@ class ApplicationController < ActionController::Base
   def is_a_number?(n)
   	n.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
   end
+
+  def set_pos_operation_token(certified_agent_id, operation_type)
+    @token = ""
+    # Wari
+    if certified_agent_id == "af478a2c47d8418a"
+      case operation_type
+      when "cash_in"
+        @token = "9cff7473"
+      when "cash_out"
+        @token = "3392eecd"
+      when "ascent"
+        @token = "573504f7"
+      end
+    end
+    # Qash
+    if certified_agent_id == "684239a94ca63639"
+      case operation_type
+      when "cash_in"
+        @token = "1f039cd8"
+      when "cash_out"
+        @token = "b87b6c80"
+      when "ascent"
+        @token = "2396b109"
+      end
+    end
+    # Smart Fidelis
+    if certified_agent_id == "99999999"
+      @has_rib = (RestClient.get "http://pay-money.net/pos/has_rib/#{@certified_agent_id}" rescue "")
+      @has_rib.to_s == "0" ? @has_rib = false : @has_rib = true
+
+      print "*****************" + @has_rib.to_s + "*****************"
+
+      if operation_type == "cash_in"
+        if @has_rib
+          @token = "5a518a5a"
+        else
+          @token = "6d6dde69"
+        end
+      end
+      if operation_type == "cash_out"
+        if @has_rib
+          @token = "2968e7b4"
+        else
+          @token = "9a28edf0"
+        end
+      end
+      if operation_type == "ascent"
+        if @has_rib
+          @token = "13a3fd04"
+        else
+          @token = "e3875eab"
+        end
+      end
+    end
+  end
 end
