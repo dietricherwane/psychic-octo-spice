2909bfc33baa58bbcd6ae9121454e00d5ed43b3b
Dietrich <dietricherwane@yahoo.fr>
2016-03-11 23:45:22 +0000

    changes

--- app/controllers/ludwin_api_controller.rb
+++ app/controllers/ludwin_api_controller.rb
@@ -664,8 +664,7 @@ class LudwinApiController < ApplicationController
 
               request.run
 
-              # Free the terminal
-              @terminal.update_attributes(busy: false)
+
 
               LudwinLog.create(operation: "Prise de pari", transaction_id: (@terminal.code rescue ""), error_code: @error_code, sent_body: body, response_body: response_body, remote_ip_address: remote_ip_address)
             end
@@ -676,6 +675,9 @@ class LudwinApiController < ApplicationController
           @error_description = 'Invalid JSON data.'
         end
       end
+
+      # Free the terminal
+      @terminal.update_attributes(busy: false)
     end
   end
 
