b15ecbf66beca48841cf1ae7ebba2a4ebbad1b6b
Dietrich <dietricherwane@yahoo.fr>
2016-05-25 23:51:43 +0000

    changes

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,3 @@
 class Log < ActiveRecord::Base
-  attr_accessible :transaction_type, :checkout_amount, :status, :error_log, :response_log, :agent, :sub_agent, :transaction_id, :fee, :remote_ip_address
+  attr_accessible :transaction_type, :checkout_amount, :status, :error_log, :response_log, :agent, :sub_agent, :transaction_id, :fee, :remote_ip_address, :status
 end
