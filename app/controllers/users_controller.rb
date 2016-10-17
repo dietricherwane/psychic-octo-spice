08474a7042c2c0e660f1d22c7560f38623e9dfa6
Dietrich <dietricherwane@yahoo.fr>
2016-10-17 20:01:18 +0000

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
         render text: %Q({"errors":[{"message":"Le mot de passe et sa confirmation ne concordent pas"}]})
       else
         @status = false
-        @user = User.new(params.merge({creation_mode_id: creation_mode.id, salt: SecureRandom.base64(8).to_s, confirmation_token: SecureRandom.hex.to_s, uuid: SecureRandom.uuid}))
+        creation_mode == '2' ? confirmed_at = DateTime.now : confirmed_at = nil
+        @user = User.new(params.merge({creation_mode_id: creation_mode.id, salt: SecureRandom.base64(8).to_s, confirmation_token: SecureRandom.hex.to_s, uuid: SecureRandom.uuid, confirmed_at: confirmed_at}))
 
         if @user.save
           @status = true
