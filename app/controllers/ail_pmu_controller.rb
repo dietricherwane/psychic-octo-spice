4f73e5219d321a10ec1e66af4867ad5f3491df2e
Dietrich <dietricherwane@yahoo.fr>
2016-11-14 13:02:20 +0000

    changes

--- app/controllers/ail_pmu_controller.rb
+++ app/controllers/ail_pmu_controller.rb
@@ -779,7 +779,7 @@ class AilPmuController < ApplicationController
           else
             success_array << notification_object.to_s
             if amount_type == "0"
-              @bet.update_attribute(:bet_status, 'Perdant') #rescue nil
+              @bet.update_attributes(bet_status: 'Perdant') #rescue nil
               validate_bet_ail("ApXTrliOp", (@bet.bet_cost_amount.to_f rescue 0), "ail_pmus")
             else
               if amount_type == "1"
