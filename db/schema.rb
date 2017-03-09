ecea274f675f2323a6a5a325d159230983f12c22
Dietrich Mensah <dietricherwane@yahoo.fr>
2017-03-09 10:32:54 +0000

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309102604) do
+ActiveRecord::Schema.define(version: 20170308145515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -668,7 +668,6 @@ ActiveRecord::Schema.define(version: 20170309102604) do
     t.string   "operation"
     t.string   "bet_status"
     t.string   "paymoney_destination_account"
-    t.string   "ticket_id"
   end
 
   create_table "game_tokens", force: true do |t|
