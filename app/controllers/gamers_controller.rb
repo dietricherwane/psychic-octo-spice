4a54d1c24e1579ffc4e2929904e1a80b4e6ea841
Dietrich <dietricherwane@yahoo.fr>
2017-05-05 17:00:11 +0000

    changes

--- app/controllers/gamers_controller.rb
+++ app/controllers/gamers_controller.rb
@@ -40,8 +40,8 @@ class GamersController < ApplicationController
       #send_data @gamers.to_csv, filename: "Parieurs-#{Date.today}.csv"
       #send_data @gamers.to_xlsx.to_stream.read, :filename => "Parieurs-#{Date.today}.xlsx", :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
     #end
-    render text: request.format
-    if params[:commit] == "Exporter"
+    #render text: request.format
+    if params[:commit] != "Exporter"
       render text: request.format
       #send_data @gamers.to_xlsx, :filename => "Parieurs-#{Date.today}.xlsx", :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
       #respond_to do |format|
